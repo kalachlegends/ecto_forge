3c0024d48f14ba6615537f407280553bed1eda7e
artem.kalachev.2002@gmail.com <artem.kalachev.2002@gmail.com>
2024-04-14 07:53:49 +0500

    fix: version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule EctoForge.MixProject do
   def project do
     [
       app: :ecto_forge,
-      version: "0.1.13",
+      version: "0.1.14",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
